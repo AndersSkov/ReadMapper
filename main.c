0a660400b9a8dfe51f21dd49eaf77180cc539af0
ChristofferCJ <christofferchangtrakuljensen@gmail.com>
2021-02-24 10:33:15 +0100

    suffix_array

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-const char* createSuffixArray(char const *input[]);
-
 int main(int argc, char const argv[])
 {
     /* code */
@@ -9,10 +7,4 @@ int main(int argc, char const argv[])
     const char suffix = createSuffixArray(input);
 
     return 0;
-}
-
-const char* createSuffixArray(char const *input[]) {
-    int SA[10];
-
-    return input;
 }
---
+++ suffix_array.c
@@ -0,0 +1,5 @@
+const char* create_suffix_array(char const *input[]) {
+    int SA[10];
+
+    return input;
+}
