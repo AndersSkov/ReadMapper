1a940099a659b7f76e84965a3de143774e665f0c
ChristofferCJ <christofferchangtrakuljensen@gmail.com>
2021-02-24 10:25:33 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+const char* createSuffixArray(char const *input[]);
+
+int main(int argc, char const argv[])
+{
+    / code /
+    char input[] = "mississippi$";
+    const char suffix = createSuffixArray(input);
+
+    return 0;
+}
+
+const char* createSuffixArray(char const *input[]) {
+    int SA[10];
+
+    return input;
+}
