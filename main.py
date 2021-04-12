4cac67242cc52df6a90ec0fce047b061425fcc17
Anders <201804890@post.au.dk>
2021-04-12 14:09:18 +0200

    SA-IS, mangler bare noget type baks ved rekrusiv kald til SA-IS med sumstring

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 import suffix_array as sa
 import bwt 
 
-input = "mississippi$"
+input = "baabaabac$"
+# baabaabac$
 
 print("naive implementation of suffix array")
 sa.naive(input)
 
-print("skew implementation of suffix array")
-sa.skew(input)
+#print("skew implementation of suffix array")
+#sa.skew(input)
 
-print("naive implementation of bwt")
-inverse = bwt.naive(input)
+#print("naive implementation of bwt")
+#inverse = bwt.naive(input)
 
-print("naive implementation of bwt_inverse")
-bwt.naive_inverse(inverse)
+#print("naive implementation of bwt_inverse")
+#bwt.naive_inverse(inverse)
 
 print("SA-IS")
-sa.SA_IS(input)
+sa.SA_IS(input, 256)
+
+
+def read_fasta_file(filename):
+    """
+    Reads the given FASTA file f and returns a dictionary of sequences.
+
+    Lines starting with ';' in the FASTA file are ignored.
+    """
+    sequences_lines = {}
+    current_sequence_lines = None
+    with open(filename) as fp:
+        for line in fp:
+            line = line.strip()
+            if line.startswith(';') or not line:
+                continue
+            if line.startswith('>'):
+                sequence_name = line.lstrip('>')
+                current_sequence_lines = []
+                sequences_lines[sequence_name] = current_sequence_lines
+            else:
+                if current_sequence_lines is not None:
+                    current_sequence_lines.append(line)
+    sequences = {}
+    for name, lines in sequences_lines.items():
+        sequences[name] = ''.join(lines)
+    return sequences
 
